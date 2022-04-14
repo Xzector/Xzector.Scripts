a9622a0d3e3b470d1eb99c8caa905fc4dc2d7f8a
Xzector <85714981+Xzector@users.noreply.github.com>
2022-04-14 17:43:04 -0500

    Delete Xzector directory

--- Xzector/_ReadMe.md
+++
@@ -1,3 +0,0 @@
-some small scripts by Z-Shuko#8287
-all rights reserved to Xzector
-date - 3/29/2022
--- Xzector/auto_block.lua
+++
@@ -1,7 +0,0 @@
-local oldindex
-oldindex = hookmetamethod(game, '__index',function(self, b)
-    if b == 'Value' and self.Name == 'Block' then
-        return false;
-    end;
-    return oldindex(self, b)
-end);
