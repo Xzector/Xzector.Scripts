2ee8c07e3513ffaecda414100c70cac888f12353
Xzector <85714981+Xzector@users.noreply.github.com>
2022-04-16 19:55:29 -0500

    Update freeze_double.lua

--- freeze_double.lua
+++ freeze_double.lua
@@ -1,4 +1,4 @@
--- credit to shuko#8287 on discord for making this, lets you freeze ur double exp and heaven time (not autoexe) compatible.
+-- credit to Z-Shuko#8287 on discord for making this, lets you freeze ur double exp and heaven time (not autoexe) compatible.
 local double = function(x)
     game.Players.LocalPlayer.Character['True']:Destroy();
     game.Players.LocalPlayer.CharacterAdded:Connect(function(x)
