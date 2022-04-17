e75b9ddc8cc0b7ebec2c0762d5f1b3e51bcb6d0d
Xzector <85714981+Xzector@users.noreply.github.com>
2022-04-16 22:13:58 -0500

    Update auto_block.lua

--- auto_block.lua
+++ auto_block.lua
@@ -1,5 +1,3 @@
--- this method is better than all the other shit methods. 
--- credit to Z-Shuko#8287 on discord for making this, lets you block without restriction, (autoexe) compatible.
 local oldindex 
 oldindex = hookmetamethod(game, '__index',function(self, b)
     if b == 'Value' and self.Name == 'Block' then
