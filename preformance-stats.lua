a08ca76ad8ec46e5f9dda7c0c0d8569bf6a13f23
Xzector <85714981+Xzector@users.noreply.github.com>
2022-07-10 01:45:41 -0500

    Delete preformance-stats.lua

--- preformance-stats.lua
+++
@@ -1,9 +0,0 @@
-local stats = function(x)
-    shared.iso = math.huge;
-    rconsolename('Preformance Tool')
-    for i = 0, shared.iso do wait(x)
-        local info = game.Stats.PerformanceStats;
-        rconsoleprint('[preformance][%s]'..info.Memory:GetValue()..'   '..info.CPU:GetValue()..'   '..info.Ping:GetValue()..'\n')
-    end;
-end;
-stats(0.5)
